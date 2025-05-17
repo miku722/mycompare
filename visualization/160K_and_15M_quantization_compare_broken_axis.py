91a4a10484916b60d770005826721f3ea0c7c7c9
huang peiwen <peiwen99722@gmail.com>
2025-05-17 11:24:18 +0800

    09-修改图片1：增加边框字体的大小的粗细

--- visualization/160K_and_15M_quantization_compare_broken_axis.py
+++ visualization/160K_and_15M_quantization_compare_broken_axis.py
@@ -13,30 +13,47 @@ x_values = df['token_num'].tolist()
 
 # 创建图像和子图
 fig = plt.figure(figsize=(10, 6))
-gs = fig.add_gridspec(2, 1, height_ratios=[1, 1.5], hspace=0.05)
+gs = fig.add_gridspec(2, 1, height_ratios=[1, 1], hspace=0.05)
 
 ax1 = fig.add_subplot(gs[0])
 ax2 = fig.add_subplot(gs[1], sharex=ax1)
 
 # 上图
-l3, = ax1.plot(x_values, y3, 'b-', marker='o', label='260K no quant')
-l4, = ax1.plot(x_values, y4, 'b-', marker='^', label='260K quant')
+l3, = ax1.plot(x_values, y3, 'b-', marker='o', markersize=10, linewidth=4, label='260K no quant')
+l4, = ax1.plot(x_values, y4, 'b-', marker='^', markersize=10, linewidth=4, label='260K quant')
 ax1.set_ylim(45, 75)
 ax1.spines['bottom'].set_visible(False)
 ax1.tick_params(labelbottom=False)
-ax1.set_ylabel("tok/s (high)")
+ax1.set_ylabel("Token/s", fontsize=12, weight='bold')
 
 # 下图
-l1, = ax2.plot(x_values, y1, 'r-', marker='o', label='15M no quant')
-l2, = ax2.plot(x_values, y2, 'r-', marker='^', label='15M quant')
+l1, = ax2.plot(x_values, y1, 'r-', marker='o', markersize=10, linewidth=4, label='15M no quant')
+l2, = ax2.plot(x_values, y2, 'r-', marker='^', markersize=10, linewidth=4, label='15M quant')
 ax2.set_ylim(0.95, 1.35)
 ax2.spines['top'].set_visible(False)
-ax2.set_ylabel("tok/s (low)")
-ax2.set_xlabel("Token Number")
+ax2.set_xlabel("Token Number", fontsize=12, weight='bold')
 
 # 设置 X 轴刻度
 ax2.set_xticks(x_values)
-ax2.set_xticklabels(x_values, rotation=45)
+ax2.set_xticklabels(x_values, rotation=45, fontsize=12, weight='bold')  # 设置字体大小
+
+# 设置 Y 轴刻度加粗
+# Y轴刻度数字加粗
+ax1.tick_params(axis='y', labelsize=12)
+ax2.tick_params(axis='y', labelsize=12)
+# 设置刻度字体加粗
+for label in ax1.get_yticklabels():
+    label.set_fontweight('bold')
+for label in ax2.get_yticklabels():
+    label.set_fontweight('bold')
+
+for label in ax2.get_xticklabels():
+    label.set_fontweight('bold')
+
+for spine in ax1.spines.values():
+    spine.set_linewidth(1.5)
+for spine in ax2.spines.values():
+    spine.set_linewidth(1.5)
 
 # 明显断轴符号
 d = 0.5
@@ -45,8 +62,7 @@ ax1.plot([0.5, 0.5], [0, 0], transform=ax1.transAxes, **kwargs)
 ax2.plot([0.5, 0.5], [1, 1], transform=ax2.transAxes, **kwargs)
 
 # 图例放入 ax1 内部右上角
-ax1.legend(handles=[l1, l2, l3, l4], loc='upper right', frameon=False)
+ax1.legend(handles=[l1, l2, l3, l4], loc='upper right', frameon=False, prop={'weight': 'bold', 'size': 12})
 
-plt.suptitle("Token Throughput vs Token Number", fontsize=14)
 plt.tight_layout()
 plt.show()
